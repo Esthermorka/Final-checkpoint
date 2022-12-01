750be7788b8f866609c50e06c7d9bf70d20188ee
Esthermorka <114414746+Esthermorka@users.noreply.github.com>
2022-12-01 15:03:10 +0100

    Update final checkpoint.sql

--- final checkpoint.sql
+++ final checkpoint.sql
@@ -22,16 +22,17 @@ create table harvrest (
 
 select * from producer
 
-select * from producer order by last_name, first_name
+select * from producer order by last_name
 
 select* from producer where region = 'Sousse'
 
 select sum(quantity) from harvest where NumW = 12
 
-select quantity, category from wine inner join harvrest on wine.NumW = harvrest.NumW
+select sum(quantity), category from harvrest inner join wine on harvrest.NumW = wine.NumW  group by category
 
-Select first_name,last_name,region,quantity from producer inner join harvrest on producer.NumP = harvrest.NumP 
-where region = 'Sousse' and quantity >= 1
+select first_name, last_name from producers inner join harvrest on producer.Nump = harvrest.NumP 
+where region = 'Sousse' and NumW >= 1 and quantity > 300 order by first_name
+
 
 select NumP, degrees from wine inner join harvrest on wine.NumW = harvrest.NumW 
 where degrees >12 and NumP = 24
